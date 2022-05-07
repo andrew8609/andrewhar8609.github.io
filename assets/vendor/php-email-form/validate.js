4c6b9f3a789f9acd05af8d805c9a6e6ca0b9d068
Andrew J. Reed <73575670+andrewjreed@users.noreply.github.com>
2022-05-06 19:30:33 -0700

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -15,7 +15,8 @@
       let thisForm = this;
 
       let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+//       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+      let recaptcha = "6LfX580fAAAAAL0PwpBL7hUztvel3Va8wdOLyw9i";
       
       if( ! action ) {
         displayError(thisForm, 'The form action property is not set!')
